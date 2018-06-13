bd164cff00a803f498183841be1caa234e06b861
AdamMacs <adammcgivern93@hotmail.co.uk>
2018-06-13 22:41:23 +0100

    fix spacing issues

--- lib/pizza_bot.rb
+++ lib/pizza_bot.rb
@@ -1,5 +1,5 @@
 class Pizzabot
-  DROP = "D"
+	DROP = "D"
 	EAST = "E"
 	WEST = "W"
 	NORTH = "N"
@@ -8,7 +8,7 @@ class Pizzabot
 	def initialize(route: [])
 		@route = route
 	end
-	
+
 	def delivery_instructions(area, drops)
 		start = Point.new(0,0)
 		drops.each do |drop|
@@ -16,13 +16,13 @@ class Pizzabot
 			deliver(start,drop)
 			@route << DROP
 			start = drop
-			end
+		end
 		@route.join
 	end
-	
-	private 
 
-  def deliver(start,drop)
+	private
+
+	def deliver(start,drop)
 		find_long_diff(start.long,drop.long)
 		find_lat_diff(start.lat,drop.lat)
 	end
@@ -30,15 +30,15 @@ class Pizzabot
 	def find_lat_diff(start,drop)
 		drop >= start ? @route << format_delivery(NORTH,drop-start) : @route << format_delivery(SOUTH,start-drop)
 	end
-	
+
 	def find_long_diff(start,drop)
 		drop >= start ? @route << format_delivery(EAST,drop-start) : @route << format_delivery(WEST,start-drop)
 	end
-	
+
 	def format_delivery(direction,num)
 		direction * num
 	end
-	
+
 	def is_valid_drop?(area,drop)
 		area.lat >= drop.lat && area.long >= drop.long
 	end
