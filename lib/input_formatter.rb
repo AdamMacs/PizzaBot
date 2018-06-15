2f7bc8b795fb342fdfebf6ceeecd4e6dddc7e5c4
Adam McGivern <adam.mcgivern@liberty-it.co.uk>
2018-06-15 19:22:23 +0100

    graceful errors

--- lib/input_formatter.rb
+++ lib/input_formatter.rb
@@ -1,15 +1,16 @@
 class InputFormatter
   require_relative 'point.rb'
-  attr_reader :grid, :drops
+  attr_reader :grid, :drops, :error
 
   def initialize(input='')
+    @error = ""
     @grid = format_input(input)
     @drops = format_drops(input)
   end
 
   def format_input(input)
     grid_size_array = input.scan(/\d\w\d/)
-    raise ArgumentError, 'You need to pass in numbers for the grid max coordinates' unless grid_size_array.any?
+    return @error << 'You need to pass in numbers for the grid max coordinates' unless grid_size_array.any?
     grid_size_array = grid_size_array.first.split('x')
     max_long = grid_size_array[0].to_i
     max_lat = grid_size_array[1].to_i
@@ -19,7 +20,7 @@ class InputFormatter
   def format_drops(input)
     drops = []
     drop_offs = input.scan(/-?\d+,\s?-?\d+/)
-    raise ArgumentError, 'You need to pass in numbers for the delivery coordinates' unless drop_offs.any?
+    return @error << 'You need to pass in numbers for the delivery coordinates' unless drop_offs.any?
     drop_offs.each do |drop_off|
       long = drop_off.split(',').first.to_i
       lat = drop_off.split(',').last.to_i
